efa5cc058478ed5fe51c405ed70207c263c570f3
Nancy Emad <nancyemadd009@gmail.com>
2025-10-12 02:44:17 +0300

    update for responsive

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,26 @@
 import './Card.css'
 
-function Card() {
+function Card(props) {
     return (
         <>
 
             <div className="card">
-                <img src="/BeautyShop.png" alt="beautyshop" />
+                <img src={props.imgCard} alt="imgCard" />
 
                 <div className="desc">
-                    <p>BeautyShop Store</p>
+                    <p>{props.title}</p>
                     <div className="links-card">
-                        <p><a href="https://beautyshop-store.vercel.app/" target='_blank'>Live demo <i class="fa-solid fa-link"></i></a></p>
-                        <p><a href="https://github.com/nancyemadd0099-collab/Beautyshop-Store" target='_blank'>Git Hub <i class="fa-solid fa-link"></i></a></p>
+                        <p>
+                            <a href={props.demoLink} target='_blank'>
+                                {props.demoText} <i className="fa-solid fa-link"></i>
+                            </a>
+                        </p>
+
+                        <p>
+                            <a href={props.githubLink} target='_blank'>
+                                {props.githubText} <i className="fa-solid fa-link"></i>
+                            </a>
+                        </p>
                     </div>
                 </div>
 
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -9,7 +9,8 @@ function Projects() {
                 <div class="container">
                     <p className='linear'>Projects</p>
                     <div class="project">
-                        <Card />
+                        <Card imgCard="/BeautyShop.png" title="BeautyShop Store" demoLink="https://beautyshop-store.vercel.app/" demoText="Live demo" githubLink="https://github.com/nancyemadd0099-collab/Beautyshop-Store" githubText="Git Hub" />
+                        <Card imgCard="/Bella Vista Restaurant.png" title="Bella Vista Restaurant" demoLink="https://bella-vista-restaurant-two.vercel.app/" demoText="Live demo" githubLink="https://github.com/nancyemadd0099-collab/Bella_Vista_Restaurant" githubText="Git Hub" />
                     </div>
                 </div>
             </section>
